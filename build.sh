50592d125797ec029bb2f5751644920ef214a5ec
blckink <86264410+blckink@users.noreply.github.com>
2025-07-12 16:32:05 +0200

    fix runtime library path

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ cargo build --release && \
 rm -rf build/partydeck-rs
 mkdir -p build/ build/res && \
 cp target/release/partydeck-rs res/PartyDeckKWinLaunch.sh build/ && \
-cp res/splitscreen_kwin.js res/splitscreen_kwin_vertical.js res/gamescope build/res
+cp res/splitscreen_kwin.js res/splitscreen_kwin_vertical.js build/res && \
+if [ -d res/gamescope ]; then cp -r res/gamescope build/res; fi && \
+libpath=$(find target/release/build -name libsteam_api.so | head -n 1) && \
+cp "$libpath" build/
